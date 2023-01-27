7144aa5355fca77ac8ee3014fbf50fbdc9566291
truongnhatanh7 <truongnhatanhw@gmail.com>
2023-01-27 17:45:58 +0700

    feat: connect to mongodb

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 const app = express();
 
-// Route files
-const bootcamps = require("./routes/bootcamps");
-
 // load Env
 dotenv.config({ path: "./config/config.env" });
+
+// Connect db
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
 
 // Dev logging middleware
 if (process.env.NODE_ENV === "development") {
@@ -20,9 +25,18 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 6969;
 
-app.listen(
+const server = app.listen(
 	PORT,
 	console.log(
 		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+			.yellow.bold
 	)
 );
+
+// Handle unhanlded prmosie rejections
+process.on("unhandledRejection", (err, promise) => {
+	console.log(`Error: ${err.message}`.red);
+	server.close(() => {
+		process.exit(1);
+	});
+});
