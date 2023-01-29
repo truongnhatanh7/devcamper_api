b7ddd7d89b4958b342e2b42730390307b4477208
truongnhatanh7 <truongnhatanhw@gmail.com>
2023-01-29 18:20:02 +0700

    feat: bootcamp owership

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -57,14 +57,25 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 // @route  PUT /api/v1/bootcamps/:id
 // @access Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-	const bootcamp = await Bootcamp.findByIdAndUpdate(
-		req.params.bid,
-		req.body,
-		{
-			new: true,
-			runValidators: true,
-		}
-	);
+	let bootcamp = await Bootcamp.findById(req.params.bid);
+
+	// Mkae sure user is bootcamp owner
+	if (
+		bootcamp.user.toString() !== req.user.id &&
+		req.user.role !== "admin"
+	) {
+		return next(
+			new ErrorResponse(
+				`The user with id ${req.user.id} is not authorized`,
+				400
+			)
+		);
+	}
+
+	bootcamp = await Bootcamp.findByIdAndUpdate(req.params.bid, req.body, {
+		new: true,
+		runValidators: true,
+	});
 
 	res.status(200).json({
 		status: true,
