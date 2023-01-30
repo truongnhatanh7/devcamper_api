a917cc17538cb2c64559f415f6d8a16a8a1c76b4
truongnhatanh7 <truongnhatanhw@gmail.com>
2023-01-30 19:03:14 +0700

    feat: limit request

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const cookieParser = require("cookie-parser");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
 
 const app = express();
 
@@ -46,6 +48,16 @@ app.use(helmet());
 
 // Prevent XSS attacks
 app.use(xss());
+
+// Rate limitting, 100 reqs/10min
+const limiter = rateLimit({
+	window: 10 * 60 * 1000, // 10 mins
+	max: 100,
+});
+
+app.use(limiter);
+
+app.use("hpp");
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
