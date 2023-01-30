0e502b3b9e39f0819a3fa614a6fdc851ce758478
truongnhatanh7 <truongnhatanhw@gmail.com>
2023-01-30 18:51:31 +0700

    feat: prevent XSS

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 const fileupload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
 
 const app = express();
 
@@ -34,6 +37,15 @@ if (process.env.NODE_ENV === "development") {
 
 // File uploading
 app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
